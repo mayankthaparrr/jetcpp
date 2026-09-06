d11418225dd452af4f02b100edc0f3b0bcfd14b0
mayankthaparrr <mayankpthapar@gmail.com>
2026-09-06 21:33:12 +0530

    Editor Layout :- Ribbon Almost Done

--- main.c
+++ main.c
@@ -6,81 +6,88 @@
 #include <stdlib.h>
 #include <raylib.h>
 #include "cmake-build-debug/_deps/raylib-src/src/raylib.h"
+#include <string.h>
+
 
 Font ribbonFont;
 Font Roboto;
-void drawRibbon();
+
+void drawRibbon2();
+
+void drawRibbon3();
+
 int main() {
+
     const char *c ="hello";
 
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(1200, 600, "Jet C++");
+    InitWindow(800, 800, "Jet C++");
     SetTargetFPS(60);
-    Color tooblue = GetColor(0x0000aaff);
     Roboto=LoadFont("C:/DEV/ATR/Roboto-Regular.ttf");
     ribbonFont=LoadFont("C:/DEV/ATR/Ac437_IBM_VGA_8x16.ttf");
 
+    Color tooblue = GetColor(0x0000aaff);
     //Loop
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(tooblue);
-        drawRibbon();
+        drawRibbon3();
         EndDrawing();
     }
     return 0;
 }
 
-void drawRibbon() {
+void drawRibbon3() {
+    int height=(int)GetScreenHeight()*0.04;
+    if (GetScreenHeight()<GetScreenWidth()) {
+        height=(int)GetScreenWidth()*0.025;
+        if (height>(GetScreenHeight())*0.4) {
+            height=(int)GetScreenHeight()*0.3;
+        }
+    }
+    printf("%d,%d\n",GetScreenWidth(),GetScreenHeight());
+    Color greyy = GetColor(0xada8acff);
+    Font usedfont=Roboto;
 
-    int screen_width = GetScreenWidth();
-    int height = GetScreenHeight();
-    Vector2 textSize;
-
-
-    //scale ribbon height w.r.t screen width
-    height=(int)height*0.04;
-
-    //Draw Ribbon BG
-    DrawRectangle(0,0,screen_width,height,GRAY);
-    //var for pos x of text
-    int posx=(int)screen_width*0.01;
-
-
+    // draw ribbon rect
+    DrawRectangle(0,0,GetScreenWidth(),height,greyy);
 
     struct Items {
         char *item;
         int width;
+        int height;
     };
+    struct Items item[9];
+    item[0].item = "File";
+    item[1].item = "Edit";
+    item[2].item = "Search";
+    item[3].item = "Run";
+    item[4].item = "Debug";
+    item[5].item = "Project";
+    item[6].item = "Options";
+    item[7].item = "Window";
+    item[8].item = "Help";
 
-    struct Items item[6];
-    item[0].item="File";
-    item[1].item="Edit";
-    item[2].item="Search";
-    item[3].item="Run";
-    item[4].item="Debug";
-    item[5].item="Project";
-    item[6].item="Options";
-
-    int totalWidth=0;
-    for (int i=0;i<=6;i++) {
-        textSize=MeasureTextEx(ribbonFont,item[i].item,height,0);
+    int usedwidth=0;
+    for (int i=0;i<9;i++) {
+        Vector2 textSize=MeasureTextEx(usedfont,item[i].item,height,0);
         item[i].width=textSize.x;
-        totalWidth+=textSize.x;
+        item[i].height=textSize.y;
+        usedwidth+=textSize.x;
     }
-        int padding=(int)screen_width/(totalWidth+5);
-        posx=padding;
-    for (int i=0;i<=6;i++) {
-        //draw text
-        posx+=(item[i].width);
-        DrawTextEx(ribbonFont,item[i].item,(Vector2){posx,1},height,0,BLACK);
+    int spaceleft=(int)GetScreenWidth()-usedwidth-(GetScreenWidth()*0.02);
+    spaceleft=(int)spaceleft/13;
+    int posx=spaceleft*2;
+
+    for (int i=0;i<9;i++) {
+        DrawTextEx(usedfont,item[i].item,(Vector2){posx,0},item[i].height,0,BLACK);
+        posx=posx+item[i].width+(spaceleft);
+        if (i==6) {
+            posx+=(spaceleft*3);
+        }
+
     }
-
-
-
-
-
-
-
 
 
 }
+
